7b05f8a51c006b1f98aa4a5ee60d9930c301c7a5
YeeeeahRight <yeeeeeah.right@gmail.com>
2020-11-06 13:24:56 +0300

    Added text reader exception

---
+++ src/main/java/com/epam/handler/exceptions/TextReaderException.java
@@ -0,0 +1,7 @@
+package com.epam.handler.exceptions;
+
+public class TextReaderException extends Exception {
+    public TextReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
